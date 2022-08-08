cf0125906b421d5f394bad886ad346a798345a60
ocs <osvaldocruzsuarez999@gmail.com>
2022-08-08 01:35:37 -0500

    se cambio la libreria de tablas y se cambio el idioma

--- Proyecto/js/datatables-simple-demo.js
+++ Proyecto/js/datatables-simple-demo.js
@@ -1,9 +1,15 @@
-window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Simple-DataTables
+  // https://github.com/fiduswriter/Simple-DataTables/wiki
+  // const datatablesSimple = document.getElementById("datatablesSimple");
+  // if (datatablesSimple) {
+  //   new simpleDatatables.DataTable(datatablesSimple, {});
+  // }
+});
+$(document).ready(function () {
+  $("#datatablesSimple").DataTable({
+    language: {
+      url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
+    },
+  });
 });
